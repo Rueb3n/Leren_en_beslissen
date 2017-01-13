76c0c76a4d660c12e884ab6f9d1c749650a876f2
Stijn Verdenius <stijn@verdenius.com>
2017-01-13 14:01:14 +0100

    products data2

---
+++ met_product.py.py
@@ -0,0 +1,21 @@
+import json
+productList = []
+
+# open file
+with open('products.json') as json_data:
+    
+    # laad data
+    data = json.load(json_data)
+    
+    # neem alleen relevante data
+    for element in data:
+        prijs = element["amount"]["format"]["round"]
+        soort = element["name"]
+        typeId = element["type"]["id"]
+        productList.append({"prijs":prijs,"soort":soort,"type":typeId})
+
+# print de eerste        
+print(productList[0])
+    
+    
+    
